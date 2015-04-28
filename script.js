69b1c420115efafb5024876b67c0173f6c57735a
Scott Bromander <scott@primeacademy.io>
2015-04-28 09:58:04 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var apikey = ''; // Put your API key here
+
+
+// Use this function to do stuff with your results. 
+// It is called after 'search' is executed.
+function searchCallback(results) {
+    console.log(results);
+}
+
+$(document).ready(function() {
+
+	// Start the search here!
+	
+});
+
+// HELPER FUNCTION
+// Executes a search using 'query' and runs searchCallback on the results of a success.
+function search(query){
+
+	$.ajax ({
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    crossDomain: true,
+	    jsonp: 'json_callback',
+	    url: 'http://www.giantbomb.com/api/search/?format=jsonp&resources=game&api_key=' + apikey +'&query=' + encodeURI(query),
+	    complete: function() {
+	        console.log('ajax complete');
+	    },
+	    success: function(data) {
+	        searchCallback(data.results);
+	    }
+	});
+
+}
